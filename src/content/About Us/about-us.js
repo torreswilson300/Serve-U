591487b6d01969a0b6350c0333711cb0b18a2fbe
Jania Henderson <jhender6@aggies.ncat.edu>
2019-09-20 12:13:33 -0400

    Update about-us.js

--- src/content/About Us/about-us.js
+++ src/content/About Us/about-us.js
@@ -1,5 +1,21 @@
+//npm<link rel="stylesheet" href="about-us.scss"></link>
+
 import React from 'react';
 const AboutPage = () => {
-  return <div>REPO PAGE</div>;
+  return <div>
+  
+
+ <h1>Mission </h1>
+   
+   <div>
+     <p>
+ We aim to create a web application that allows individuals to conveniently
+  yet efficiently log service hours, access various community service events, 
+  share ideas with organizations, allow leaders to sign off and on hours, and more.
+  </p>
+ 
+ </div>
+   
+    </div>;
 };
 export default AboutPage;
