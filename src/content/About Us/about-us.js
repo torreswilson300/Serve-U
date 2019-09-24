2a3bd9f4516332ecaffa3f9671b8e56f83f840e8
Jania Henderson <jhender6@aggies.ncat.edu>
2019-09-24 11:50:57 -0400

    About us

--- src/content/About Us/about-us.js
+++ src/content/About Us/about-us.js
@@ -4,17 +4,23 @@ import React from 'react';
 const AboutPage = () => {
   return <div>
   
+  <title> Serve U - About Us </title>
 
- <h1>Mission </h1>
-   
+  <div className="class-example" style={{width: '500px', height: '200px'}}>
+  <div style={{backgroundColor: "#0073CF"}}>
+
+ <h1> <strong> OUR MISSION </strong> </h1>
+ </div>
    <div>
      <p>
  We aim to create a web application that allows individuals to conveniently
   yet efficiently log service hours, access various community service events, 
   share ideas with organizations, allow leaders to sign off and on hours, and more.
   </p>
+
+  </div>
  
- </div>
+  </div> 
    
     </div>;
 };
