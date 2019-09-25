bea0be9dfba72669112fa059a6771f66e2ac6c8b
torreswilson300 <tcwilson@aggies.ncat.edu>
2019-09-24 22:36:01 -0400

    home page for demo

---
+++ src/content/Organizations/organization-page.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+const OrgPage = () => {
+  return (
+    <div className="bx--grid bx--grid--full-width">
+
+    
+    <div className="bx--row">
+      <div className="bx--col-sm-1">1/4</div>
+      <div className="bx--col-sm-1">1/4</div>
+      <div className="bx--col-sm-1">1/4</div>
+      <div className="bx--col-sm-1">1/4</div>
+    </div>
+  </div>
+  );
+};
+export default OrgPage;
