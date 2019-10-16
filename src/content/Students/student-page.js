0e0868134b428f6ff8b2ca981f983ba7e37597ee
torreswilson300 <tcwilson@aggies.ncat.edu>
2019-10-16 14:07:51 -0400

    add link to sign in form

--- src/content/Students/student-page.js
+++ src/content/Students/student-page.js
@@ -1,11 +1,22 @@
-import React from "react";
-import { TextInput, Button, Form, TextArea } from "carbon-components-react";
+import React, { Component, useRef, useEffect }  from "react";
+import { TextInput, Button, Form, TextArea, HeaderMenuItem } from "carbon-components-react";
 import "./student-page.scss";
+import { Route, Link } from "react-router-dom";
+
+
+const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)   
+const useMountEffect = (fun) => useEffect(fun, [])
 
 const StudentPage = () => {
+  const myRef = useRef(null)
+
+	useMountEffect(() => scrollToRef(myRef))
+
   return (
     <div className="bx--grid bx--grid--full-width">
       <div className="bx--row" id="row-1">
+        <p id="top-left">If students want to sign up for Serve U services. <br/>Click Here!</p>
+      <Button onClick={()=>scrollToRef(myRef)} id="top-left2" className="bx--btn bx--btn--danger bx--btn--sm">Student Sign Up!!</Button>
         <img
           className=" bx--col-lg-16"
           src={require("./student-org-pic.jpg")}
@@ -14,20 +25,22 @@ const StudentPage = () => {
           align="middle"
           alt="serve"
         />
+       
       </div>
       <div className="bx--row" id="row-2">
+      
         <div className="bx--offset-lg-2 bx--col-lg-12">
           <p className="p-row-2">
             Students ensure that our community is being cared for. Students are
             the powerhouse of any organization. The constant effort of NCAT
-            students to
+            students to give back to our community is of much value.
           </p>
         </div>
       </div>
 
-      <div className="bx--row" id="row-3">
+      <div className="bx--row" id="row-3" ref={myRef}>
         <div className="bx--offset-lg-1 bx--col-lg-8">
-          <Form className="org-form">
+          <Form id="student-form">
             <TextInput
               className="form-input"
               id="Org-Name"
@@ -60,7 +73,7 @@ const StudentPage = () => {
               labelText="Student Description"
               placeholder="In a few words, describe your interests..."
             ></TextArea>
-            <Button className="button"> Confirm Organization Sign Up </Button>
+            <Button className="button"> Confirm Student Sign Up </Button>
           </Form>
         </div>
       </div>
