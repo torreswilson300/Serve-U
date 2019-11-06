4ff97c7550d4ea74a9e326882978ae8e5f60aaa9
torreswilson300 <tcwilson@aggies.ncat.edu>
2019-11-06 14:22:12 -0500

    profile page hard code

--- src/content/Profile/profile.js
+++ src/content/Profile/profile.js
@@ -1,11 +1,68 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import Avatar from "react-avatar-edit";
+import "./profile.scss";
 
 class Profile extends Component {
-    render(){
-        return(
-            <p>profile </p>
-        );
+  constructor(props) {
+    super(props);
+
+    const src = "einstein.jpg";
+    this.state = {
+      preview: null,
+      src,
+      isLoggedIn: false
+    };
+  }
+
+  onImageChange = event => {
+    if (event.target.files && event.target.files[0]) {
+      let reader = new FileReader();
+      reader.onload = e => {
+        this.setState({ image: e.target.result });
+      };
+      reader.readAsDataURL(event.target.files[0]);
     }
-    
+  };
+
+  render() {
+    return (
+      <div class="bx--grid bx--grid--full-width">
+        <div class="bx--row">
+          <div className="bx--col-lg-16">
+            <h1 align="center">Profile Page</h1>
+          </div>
+        </div>
+        <div class="bx--row">
+          <div className="bx--col-lg-16">
+            <input
+              type="file"
+              onChange={this.onImageChange}
+              className="filetype"
+              id="group_image"
+            />
+            <img id="target" src={this.state.image} alt="preview" width="150"/>
+
+            
+          </div>
+        </div>
+        <div class="bx--row">
+          <div className="bx--col-lg-16">
+            <h2 align="center">List of student orgs</h2>
+            <p align="center">Association for Computing Machinery</p>
+            <p align="center">NSBE</p>
+            <p align="center">SWE</p>
+          </div>
+        </div>
+        <div class="bx--row">
+          <div className="bx--col-lg-16">
+            <h2 align="center">Number of ours for each org</h2>
+            <p align="center">Association for Computing Machinery: 16hrs</p>
+            <p align="center">NSBE: 12hrs</p>
+            <p align="center">SWE: 6hrs</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Profile;
--- src/content/Sign In Landing Page/sign-in.js
+++ src/content/Sign In Landing Page/sign-in.js
@@ -1,79 +1,68 @@
 import { TextInput, Button, Form, TextArea } from "carbon-components-react";
-import React, {Component} from "react";
-class SignInPage extends Component{
-
-    state = {
-      demos: [],
-      demoorg: {
-        Username: 'Username',
-        OrgName: 'OrgName', 
-        Email: 'Email',
-        Password: 'Password',
-        Description: 'Description',
-      }
+import React, { Component } from "react";
+class SignInPage extends Component {
+  state = {
+    demos: [],
+    demoorg: {
+      Username: "Username",
+      OrgName: "OrgName",
+      Email: "Email",
+      Password: "Password",
+      Description: "Description"
     }
+  };
 
-    onSubmit = () => {
-      alert("User has been added!");
-  
-      const { demo } = this.state;
-      fetch(
-        `http://localhost:4000/users/addUser?Username=${this.state.Username}&FirstName=${this.state.FirstName}&LastName=${this.state.LastName}&Email=${this.state.Email}&Password=${this.state.Password}&Description=${this.state.Description}`
-      )
-        .then(this.getUsers)
-        .catch(err => console.error(err));
-    };
+  onSubmit = () => {
+    alert("User has been added!");
 
-    render(){
-  return (
+    const { demo } = this.state;
+    fetch(
+      `http://localhost:4000/users/addUser?Username=${this.state.Username}&FirstName=${this.state.FirstName}&LastName=${this.state.LastName}&Email=${this.state.Email}&Password=${this.state.Password}&Description=${this.state.Description}`
+    )
+      .then(this.getUsers)
+      .catch(err => console.error(err));
+  };
 
+  render() {
+    return (
+      <div className="bx--row" id="row-3">
+        <div className="bx--offset-lg-2 bx--col-lg-8">
+          <div class="bx--rows">
+            <div class="bx--rows">Student Sign-Up</div>
+            <Form className="org-form">
+              <TextInput
+                value={this.state.Username}
+                onChange={e => this.setState({ Username: e.target.value })}
+                className="form-input"
+                id="Org-Name"
+                labelText="Username"
+                placeholder="Username"
+              />
+              <TextInput
+                value={this.state.Password}
+                onChange={e => this.setState({ Password: e.target.value })}
+                className="form-input"
+                id="Org-Pass"
+                type="password"
+                required
+                labelText="Student Password"
+                placeholder="Password"
+              />
 
-    <div className="bx--row" id="row-3">
-    <div className="bx--offset-lg-2 bx--col-lg-8"
-    
-    
-    >
+              <Button onClick={() => this.onSubmit()} className="button">
+                {" "}
+                Confirm Student Sign Up{" "}
+              </Button>
+            </Form>
+          </div>
 
+          <div class="bx--rows"></div>
 
-      <div class="bx--rows"
-      >
-      <div class="bx--rows">Student Sign-Up</div>
-        <Form className="org-form"
-        >
-                  <TextInput
-                    value={this.state.Username}
-                    onChange={e => this.setState({ Username: e.target.value })}
-                    className="form-input"
-                    id="Org-Name"
-                    labelText="Username"
-                    placeholder="Username"
-                    
-                  />
-                  <TextInput
-                    value={this.state.Password}
-                    onChange={e => this.setState({ Password: e.target.value })}
-                    className="form-input"
-                    id="Org-Pass"
-                    type="password"
-                    required
-                    labelText="Student Password"
-                    placeholder="Password"
-                  />
-
-                  <Button onClick={() => this.onSubmit()} className="button">
-                    {" "}
-                    Confirm Student Sign Up{" "}
-                  </Button>
-                </Form>
+          <div class="bx--social"></div>
+        </div>
       </div>
-
-      <div class="bx--rows"></div>
-
-      <div class="bx--social"></div>
-    </div>
-  </div>
-);
-};
+    );
+  }
 }
 
 export default SignInPage;
