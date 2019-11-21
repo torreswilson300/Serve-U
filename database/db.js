05ce54a8f65e4e38d02659f376ff7c526b92f17a
Jania Henderson <jhender6@aggies.ncat.edu>
2019-11-20 23:27:08 -0500

    latest

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const db = {}
-const sequelize = new Sequelize('serveu', 'root', 'Pr0T3chTioN!?', {
+const sequelize = new Sequelize('serveu', 'root', 'icecream2@W', {
   host: 'localhost',
   dialect: 'mysql',
   operatorsAliases: false,
