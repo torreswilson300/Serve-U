4c8bd19f34e3aaf446f5f93c6db131852d922e67
Dontavius03 <dontaviushopkins@ymail.com>
2019-12-07 17:42:54 -0500

    Orgs can view Students

--- routes/Organizations.js
+++ routes/Organizations.js
@@ -51,14 +51,18 @@ router.get('/dash', (req,res) => {
         hbsContent.id = req.session.user.id;
         hbsContent.orgName = req.session.user.orgName;
     }
-    Organization.findAll({})
+    Organization.findOne({ where: { email: hbsContent.email}})
     .then(orgs => {
-        res.render('dash', {
+        // console.log(orgs);
+        res.render('dash',{
+            orgName: orgs.orgName,
+            email: orgs.email,
+            numOfPost: orgs.numOfPost,
+            description: orgs.description,
             isOrg: hbsContent.isOrg,
-            loggedin: hbsContent.loggedin,
-            orgs:orgs
+            loggedin: hbsContent.loggedin
         });
-    })  
+    })   
 
 })
 //Display add Org Form
@@ -161,7 +165,7 @@ router.post('/post', (req,res) => {
                         org,
                         organizationId
                     })
-                    .then(res.redirect('/orgs/list'))
+                    .then(res.redirect('/orgs/dash'))
                     .catch(err => console.log(err));
                 
                     
@@ -263,6 +267,26 @@ router.get('/' , (req, res) => {
 //Route to About Page
 router.get('/about' , (req, res) => {
     res.render('about' , hbsContent)})
+
+
+//This shows a list of students for a particular Post
+router.get('/viewStudents', (req,res) => {
+    var id = req.session.user.id
+    //Get students for a given Post
+Organization.findByPk(id,{include: ['student']})
+.then((org) => {
+    var o = []
+    o = org.student
+    console.log(o)
+
+        res.render('viewStudents',{
+            o:o,
+            isStudent: hbsContent.isOrg,
+            loggedin: hbsContent.loggedin})
+            //console.log(s)
+    })       
+})
+
 
 
 module.exports = router;
